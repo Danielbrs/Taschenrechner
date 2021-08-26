f619a518d1e8192e132c4e295fa618d4ff2d8d4c
Daniel Boros / BBS2H20A <Daniel.Boros@edu.bib.de>
2021-08-26 09:33:21 +0200

    Zahlen werden im Label angezeigt beim anklicken

--- Taschenrechner/ViewController.swift
+++ Taschenrechner/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    @IBOutlet weak var ausgabefeld: UILabel!
+    
+    
+    @IBAction func zahlenButton(_ sender: UIButton) {
+        ausgabefeld.text = sender.currentTitle
+    }
+    
+    
+    
+    @IBAction func operatorenButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func kommaButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func loeschButton(_ sender: UIButton) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
