03990d8f746ed8c7802b8600bbf35356619f2ad2
Daniel Boros / BBS2H20A <Daniel.Boros@edu.bib.de>
2021-08-30 09:19:56 +0200

    Rechnung mit Operatoren fast möglich

--- Taschenrechner/ViewController.swift
+++ Taschenrechner/ViewController.swift
@@ -12,17 +12,52 @@ class ViewController: UIViewController {
     
 
     @IBOutlet weak var ausgabefeld: UILabel!
+    var nummerEingeben = false
+    var ersteZahl = 0
+    var zweiteZahl = 0
+    var operation = " "
     
     
     @IBAction func zahlenButton(_ sender: UIButton) {
         ausgabefeld.text = sender.currentTitle
+        let zahl = sender.currentTitle!
+        
+        if nummerEingeben{
+            ausgabefeld.text = ausgabefeld.text! + zahl
+        } else{
+            ausgabefeld.text = zahl
+            nummerEingeben = true
+        }
+        
     }
     
     
     
     @IBAction func operatorenButton(_ sender: UIButton) {
-    }
+        nummerEingeben = false
+        ersteZahl = Int(ausgabefeld.text!)!
+        operation = sender.currentTitle!
+        
+            }
     
+    @IBAction func gleichZeichen(_ sender: UIButton) {
+        nummerEingeben = false
+        var ergebnis = 0
+        zweiteZahl = Int(ausgabefeld.text!)!
+        
+        if operation == "+"{
+            ergebnis = ersteZahl + zweiteZahl
+        } else if operation == "-" {
+            ergebnis = ersteZahl - zweiteZahl
+        } else if operation == "x" {
+            ergebnis = ersteZahl * zweiteZahl
+        } else if operation == "/" {
+            ergebnis = ersteZahl / zweiteZahl
+        }
+        
+        ausgabefeld.text = String(ergebnis)
+        
+    }
     
     
     @IBAction func kommaButton(_ sender: UIButton) {
